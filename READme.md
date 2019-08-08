77be5181d238afd497e0a104c9b01f384b095379
Yuulya <fizz.uet@gmail.com>
2019-08-08 14:00:33 +0700

    Auto check in with puppeteer

---
+++ READme.md
@@ -0,0 +1,11 @@
+I. Config username and password in .env file
+
+II. Install pm2 global by run this command
+```handlebars
+   npm i pm2 -g
+```
+III. Enter yuu_runner directory and run script after pm2 was installed successfully
+```handlebars
+   pm2 start app.json
+```
+IV. Done!
---
+++ puppet.js
@@ -0,0 +1,40 @@
+require('dotenv').config();
+const puppeteer = require('puppeteer');
+
+const username = process.env.YUU_USER;
+const password = process.env.YUU_PASSWORD;
+
+run().then(r => {
+	console.log("Run ok")
+});
+
+async function run() {
+	const browser = await puppeteer.launch();
+	const page = await browser.newPage();
+
+	await page.goto('https://auth.garena.com/oauth/login?client_id=200064&response_type=token&redirect_uri=https%3A%2F%2Fthoitrang.bns.garena.vn%2Fconnect%2Fgarena%2Fcallback')
+	await page.type('#sso_login_form_account', username);
+	await page.type('#sso_login_form_password', password);
+	await page.click('#confirm-btn');
+
+	await page.waitForNavigation({waitUntil : "networkidle2"});
+	await page.screenshot({path: 'example.png'});
+
+	const textOfFirstDiv = await page.evaluate(
+		() => document.querySelector('div[class=login] a').textContent
+	);
+
+	console.log(textOfFirstDiv);
+
+	// Click "Nhận" button
+	await page.click('.middle');
+	const textOfFirstDiv2 = await page.evaluate(
+		() => document.querySelector('.swal2-actions').textContent
+	);
+
+	// Click "Confirm" button
+	await page.click('.swal2-confirm');
+	await browser.close();
+
+	setTimeout(run, 300000)
+}
