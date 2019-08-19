94afd98b4082f99f634791321b398e302e7e04f1
Yuulya <fizz.uet@gmail.com>
2019-08-19 13:53:35 +0700

    Add try catch

--- puppet.js
+++ puppet.js
@@ -9,32 +9,37 @@ run().then(r => {
 });
 
 async function run() {
-	const browser = await puppeteer.launch();
-	const page = await browser.newPage();
+	try {
+		const browser = await puppeteer.launch();
+		const page = await browser.newPage();
 
-	await page.goto('https://auth.garena.com/oauth/login?client_id=200064&response_type=token&redirect_uri=https%3A%2F%2Fthoitrang.bns.garena.vn%2Fconnect%2Fgarena%2Fcallback')
-	await page.type('#sso_login_form_account', username);
-	await page.type('#sso_login_form_password', password);
-	await page.click('#confirm-btn');
+		await page.goto('https://auth.garena.com/oauth/login?client_id=200064&response_type=token&redirect_uri=https%3A%2F%2Fthoitrang.bns.garena.vn%2Fconnect%2Fgarena%2Fcallback')
+		await page.type('#sso_login_form_account', username);
+		await page.type('#sso_login_form_password', password);
+		await page.click('#confirm-btn');
 
-	await page.waitForNavigation({waitUntil : "networkidle2"});
-	await page.screenshot({path: 'example.png'});
+		await page.waitForNavigation({waitUntil: "networkidle2"});
+		await page.screenshot({path: 'example.png'});
 
-	const textOfFirstDiv = await page.evaluate(
-		() => document.querySelector('div[class=login] a').textContent
-	);
+		const textOfFirstDiv = await page.evaluate(
+			() => document.querySelector('div[class=login] a').textContent
+		);
 
-	console.log(textOfFirstDiv);
+		console.log(textOfFirstDiv);
 
-	// Click "Nhận" button
-	await page.click('.middle');
-	const textOfFirstDiv2 = await page.evaluate(
-		() => document.querySelector('.swal2-actions').textContent
-	);
+		// Click "Nhận" button
+		await page.click('.middle');
+		const textOfFirstDiv2 = await page.evaluate(
+			() => document.querySelector('.swal2-actions').textContent
+		);
 
-	// Click "Confirm" button
-	await page.click('.swal2-confirm');
-	await browser.close();
+		// Click "Confirm" button
+		await page.click('.swal2-confirm');
+		await browser.close();
 
-	setTimeout(run, 300000)
+		setTimeout(run, 300000)
+	} catch (e) {
+		console.log(e);
+		setTimeout(run, 300000)
+	}
 }
